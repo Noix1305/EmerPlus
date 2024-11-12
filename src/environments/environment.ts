d52749bb47aec76b060a462fb5ffdfbffa044a62
Jose Pino Araya <j.pino2610@gmail.com>
2024-11-11 23:46:06 -0300

    resend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    API_URL: '',
-    API_KEY_SUPABASE: '',
-    CORREO_USER_DB: '',
-    PASSWORD_DB: ''
+    API_URL: 'https://ndmnmgusnnmndqwigiyp.supabase.co',
+    API_KEY_SUPABASE: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5kbW5tZ3Vzbm5tbmRxd2lnaXlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjczOTExNzEsImV4cCI6MjA0Mjk2NzE3MX0.ESF_jBHTyESdtYoCWIHv9uo-hX4caAtGiCbstBf3ITc',
+    CORREO_USER_DB: 'admin@gmail.com',
+    PASSWORD_DB: '!a%i&dmi#m0e%sn123'
   };
