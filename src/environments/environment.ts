9c915c610d114bde4cd54b0b92a1ef27c752344f
gamer <95480971+NOMAUR@users.noreply.github.com>
2025-06-05 14:44:23 -0400

    n

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,11 @@
 export const environment = {
   production: false,
-  API_URL: '',
-  API_KEY_SUPABASE: '',
+  API_URL: 'https://yjapwpqwuofrihkzehaz.supabase.co',
+  API_KEY_SUPABASE: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlqYXB3cHF3dW9mcmloa3plaGF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQwNjgzMDAsImV4cCI6MjA1OTY0NDMwMH0.qsW0dBkL7wJBFQxuiYvrQdsDhBGFA_I3HxRP4vWP06g',
   CORREO_USER_DB: '',
   PASSWORD_DB: '',
   GOOGLE_MAP_TOKEN: '',
-  ENCRYPT_KEY: ''
+  ENCRYPT_KEY: 'emerplus2024'
 };
+
+
